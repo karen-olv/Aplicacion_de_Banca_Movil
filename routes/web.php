dd4db5b5a76fb22f48948620e102eb4f7c5c1f1e
Angelsan22 <123048340@upq.edu.mx>
2025-10-18 00:25:19 -0600

    Se realizaron las rutas correspondientes de cada controlador

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,18 @@ Route::get('/', function () {
 });
 
 Route::get('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/security', [AuthController::class, 'security'])->name('security');
+
+Route::get('/home', [home::class, 'home'])->name('home');
+
+Route::get('/layouts/app', [layouts::class, 'app'])->name('app');
+Route::get('/layouts/cards', [layouts::class, 'card'])->name('layouts.cards');
+
+Route::get('/support', [support::class, 'support'])->name('support');
+
+Route::get('/transactions/loans', [transactions::class, 'loans'])->name('transactions.loans');
+Route::get('/transactions/qr-payments', [transactions::class, 'qr'])->name('transactions.qr');
+Route::get('/transactions/transfer', [transactions::class, 'transfer'])->name('transactions.transfer');
+
+Route::get('/users/account', [user::class, 'account'])->name('users.account');
