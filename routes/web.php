6a87910e8648449f92d49b92441d06429bede805
Daniellicea <123047951@upq.edu.mx>
2025-10-17 21:52:31 -0600

    controlador
    controlador de login

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\AuthController;
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-
-Route::get('/login', function () {
-    return view('auth.login');
-});
-
-
+Route::get('/login', [AuthController::class, 'login'])->name('login');
